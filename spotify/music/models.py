b1c57573fe5e9f0bbfc192138887b0dd896ee1f8
Marya Kim <markim1815@gmail.com>
2022-10-15 10:07:44 -0400

    Implement Album, Song models, serializers, views

--- spotify/music/models.py
+++ spotify/music/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Album(models.Model):
+    title = models.CharField(max_length=250)
+    artist = models.CharField(max_length=250)
+    description = models.TextField(null=True)
+    #released = models.DateTimeField()
+
+class Song(models.Model):
+    title = models.CharField(max_length=250)
+    artist = models.CharField(max_length=250)
+    album_id = models.ForeignKey(Album, related_name='songs', on_delete = models.CASCADE)
+
+
+    def __str__(self):
+        return '%s' % (self.title)
--- spotify/music/views.py
+++ spotify/music/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .serializers import AlbumSerializer, AlbumSubSerializer, SongSerializer
+from .models import Album, Song
+
+@api_view(['POST'])
+def create_album(request):
+    serializer = AlbumSubSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_album_by_id(request, album_id):
+    try:
+        queryset = Album.objects.get(id=album_id)
+    except Album.DoesNotExist:
+        return JsonResponse({"error":"does not exist"}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = AlbumSerializer(queryset)
+    return JsonResponse(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def create_song(request):
+    serializer = SongSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+    return JsonResponse(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_all_songs(request):
+    queryset = Song.objects.all()
+    serializer = SongSerializer(queryset, many=True)
+    return JsonResponse(serializer.data)
+
+
+
+
+
